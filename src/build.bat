5e2af32b513379de5c065e537f0a02346abf6a74
Marley Gray <marleyg@microsoft.com>
2023-02-15 17:40:11 -0600

    Deployment fixes
    ASPNETCORE in configuration!

--- src/build.bat
+++ src/build.bat
@@ -1,2 +1,2 @@
-docker build -t ecsdemos.azurecr.io/pathfinderfx:1.0.0.0 .
-docker push ecsdemos.azurecr.io/pathfinderfx:1.0.0.0
+docker build -t ecsdemos.azurecr.io/pathfinderfx:1.0.0.4 .
+docker push ecsdemos.azurecr.io/pathfinderfx:1.0.0.4
