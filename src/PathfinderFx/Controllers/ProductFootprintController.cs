4c5cfe1dfbf7186deda5cc83a31083629172d6b4
Marley Gray <marleyg@microsoft.com>
2023-02-16 16:19:46 -0600

    Adding events and v2 paths

--- src/PathfinderFx/Controllers/ProductFootprintController.cs
+++ src/PathfinderFx/Controllers/ProductFootprintController.cs
@@ -3,8 +3,10 @@ using PathfinderFx.Model;
 
 namespace PathfinderFx.Controllers
 {
+    //[Authorize]
     [ApiController]
-    [Route("footprints")]
+    [Route("/2/")]
+    [Produces("application/json")]
     public class ProductFootprintController : ControllerBase
     {
 
@@ -15,16 +17,40 @@ namespace PathfinderFx.Controllers
             _logger = logger;
         }
 
-        [HttpGet]
+        /// <summary>
+        /// Retrieves available footprints for the authenticated user. You can set a limit to the number of footprints returned and filter the results by product name.
+        /// </summary>
+        /// <param name="limit"></param>
+        /// <param name="filter"></param>
+        /// <returns>ProductFootprints</returns>
+        /// <remarks>
+        /// Get footprints with CPC code "3342":
+        ///     $filter=productCategoryCpc eq '3342'
+        ///
+        /// Get footprints scoped for country:
+        ///     $filter=pcf/geographyCountry eq 'DE'
+        ///
+        /// Get footprints for 2023 reporting period:
+        ///     $filter=(pcf/reportingPeriodStart ge '2023-01-01T00:00:00.000Z') and (pcf/reportingPeriodStart lt '2024-01-01T00:00:00.000Z') and (pcf/reportingPeriodEnd ge '2023-01-01T00:00:00.000Z') and (pcf/reportingPeriodEnd lt '2024-01-01T00:00:00.000Z')
+        ///
+        /// Get footprints for a specific product:
+        ///     $filter=productIds/any(productId:(productId eq 'urn:...'))
+        /// </remarks>
+        [HttpGet ("footprints")]
         [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(ProductFootprints))]
         [ProducesResponseType(StatusCodes.Status403Forbidden)]
-        public ProductFootprints ListFootprints()
+        public ProductFootprints ListFootprints(int limit = 0, string filter = "")
         {
             _logger.LogInformation("Getting footprints");
             return ProductFootprints.FromJson(System.IO.File.ReadAllText("Data/pfsv2.json"));
         }
 
-        [HttpGet("{id}")]
+        /// <summary>
+        /// Retrieves a specific footprint by id.
+        /// </summary>
+        /// <param name="id">UUID/GUID</param>
+        /// <returns>ProductFootprint</returns>
+        [HttpGet("footprints/{id}")]
         [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(ProductFootprints))]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [ProducesResponseType(StatusCodes.Status403Forbidden)]
@@ -34,5 +60,19 @@ namespace PathfinderFx.Controllers
             return ProductFootprints.FromJson(System.IO.File.ReadAllText("Data/pfv2.json"));
         }
         
+        [HttpPost("events")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(EmptyResult))]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(NotImplementedResult))]
+        //[Consumes("Content-Type: application/cloudevents+json; charset=UTF-8")]
+        public void ActionEvent(PfRequestEvent actionEvent)
+        {
+            _logger.LogInformation("Action Event: {EventBody}", "actionEvent passed in");
+            throw new NotImplementedException();
+        }
     }
+
+
+
+
 }
---
+++ src/PathfinderFx/Model/PfRequestEvent.cs
@@ -0,0 +1,53 @@
+using System.Text.Json.Serialization;
+
+namespace PathfinderFx.Model;
+
+public class PfRequestEvent
+{
+    public PfRequestEvent(string source)
+    {
+        Source = source;
+        Type = "org.wbcsd.pathfinder.ProductFootprintRequest.Created.v1";
+        SpecVersion = "1.0";
+        Id = new Guid().ToString();
+        Time = DateTime.UtcNow.ToString("yyyy-MM-ddTHH:mm:ssZ");
+        Data = new PfRequestData();
+    }
+    
+    [JsonPropertyName("type")]
+    public string Type { get; set; }
+    
+    [JsonPropertyName("specVersion")]
+    public string SpecVersion { get; set; }
+    
+    [JsonPropertyName("id")]
+    public string Id { get; set; }
+    
+    [JsonPropertyName("source")]
+    public string Source { get; set; }
+    
+    [JsonPropertyName("time")]
+    public string Time { get; set; }
+    
+    [JsonPropertyName("data")]
+    public PfRequestData Data { get; set; }
+    
+}
+
+public class PfRequestData
+{
+    public PfRequestData()
+    {
+    }
+    public PfRequestData(ProductFootprints pf, string comment)
+    {
+        Pf = pf;
+        Comment = comment;
+    }
+
+    [JsonPropertyName("productFootprintFragment")]
+    public ProductFootprints Pf { get; set; }
+    
+    [JsonPropertyName("comment")]
+    public string Comment { get; set; }
+}
