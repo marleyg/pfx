281756bcf687eb58aafce64d143ba2101762be6e
Marley Gray <marleyg@microsoft.com>
2023-03-01 08:52:38 -0600

    Update build.bat

--- src/build.bat
+++ src/build.bat
@@ -1,2 +1,2 @@
-docker build -t ecsdemos.azurecr.io/pathfinderfx:1.0.0.5 .
-docker push ecsdemos.azurecr.io/pathfinderfx:1.0.0.5
+docker build -t ecsdemos.azurecr.io/pathfinderfx:1.0.0.6 .
+docker push ecsdemos.azurecr.io/pathfinderfx:1.0.0.6
