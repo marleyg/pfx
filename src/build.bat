e31ab31b4d59f82048c63479df7dca148836df16
Marley Gray <marleyg@microsoft.com>
2023-05-04 08:22:50 -0500

    Update build.bat

--- src/build.bat
+++ src/build.bat
@@ -1,2 +1,3 @@
+rem az account set --subscription 32886bdb-91b8-4941-96c9-a662977d4455
 docker build -t ecsdemos.azurecr.io/pathfinderfx:1.0.0.7 .
 docker push ecsdemos.azurecr.io/pathfinderfx:1.0.0.7
