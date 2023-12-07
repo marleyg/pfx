812d4d984957de30138c42bf4c1d82a271408af1
Marley Gray <marleyg@microsoft.com>
2023-12-07 13:43:04 -0600

    Updates

--- src/PathfinderFx/Model/PfResponseEvent.cs
+++ src/PathfinderFx/Model/PfResponseEvent.cs
@@ -2,47 +2,30 @@ using System.Text.Json.Serialization;
 
 namespace PathfinderFx.Model;
 
+
 public class PfResponseEvent
 {
     [JsonPropertyName("type")]
-    public string Type { get; set; }
-    
+    public string Type { get; set; } = "org.wbcsd.pathfinder.ProductFootprintRequest.Fulfilled.v1";
+
     [JsonPropertyName("specVersion")]
-    public string SpecVersion { get; set; }
-    
+    public string SpecVersion { get; set; } = "1.0";
+
     [JsonPropertyName("id")]
-    public string Id { get; set; }
-    
+    public string Id { get; set; } = new Guid().ToString();
+
     [JsonPropertyName("source")]
     public string Source { get; set; }
-    
+
     [JsonPropertyName("data")]
     public PfResponseData Data { get; set; }
-    
-    public PfResponseEvent(string source, string requestEventId, ProductFootprints pfs)
-    {
-        Source = source;
-        Type = "org.wbcsd.pathfinder.ProductFootprintRequest.Fulfilled.v1";
-        SpecVersion = "1.0";
-        Id = new Guid().ToString();
-        Data = new PfResponseData(requestEventId, pfs);
-    }
 }
 
 public class PfResponseData
 {
-    public PfResponseData(string requestEventId, ProductFootprints pfs)
-    {
-        RequestEventId = requestEventId;
-        Pf = pfs;
-    }
-    public PfResponseData()
-    {
-    }
-    
     [JsonPropertyName("pfs")]
     public ProductFootprints Pf { get; set; }
-    
+
     [JsonPropertyName("requestEventId")]
     public string RequestEventId { get; set; }
 }
