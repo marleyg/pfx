54af5e84fe500346fad422f52f7137a50f889991
Marley Gray <marleyg@microsoft.com>
2023-12-08 14:33:18 -0600

    Update webappLoadCert.sh

--- src/CertGenerator/webappLoadCert.sh
+++ src/CertGenerator/webappLoadCert.sh
@@ -1,1 +1,2 @@
+#https://brianmeeker.me/2022/08/29/setting-up-abp-with-openiddict-on-azure-app-services/
 az webapp config appsettings set --name PathfinderFx --resource-group ecs-sandbox --settings WEBSITE_LOAD_CERTIFICATES=F3E5DF0299F44E925F6A683BD1D7136C2F6D84D6,A3C0822FC6B8773AB4331EE48CD3E7B0EECCCA3A
