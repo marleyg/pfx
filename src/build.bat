5be4feb4ddf5a1359fde63782fd63aee279b1d53
Marley Gray <marleyg@microsoft.com>
2024-03-25 10:43:49 -0500

    remove

--- src/build.bat
+++
@@ -1,3 +0,0 @@
-rem az account set --subscription 32886bdb-91b8-4941-96c9-a662977d4455
-docker build -t ecsdemos.azurecr.io/microsoft/pathfinderfx:1.1.1.15 .
-docker push ecsdemos.azurecr.io/microsoft/pathfinderfx:1.1.1.15
